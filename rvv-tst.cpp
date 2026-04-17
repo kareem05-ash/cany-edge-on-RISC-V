ec1205dc7a2019ede243028168b8246082ece283
kareem05-ash <kareem.ash05@gmail.com>
2026-04-18 01:12:15 +0200

    edit rvv-tst.cpp: add no. of iterations log

--- rvv-tst.cpp
+++ rvv-tst.cpp
@@ -9,8 +9,10 @@ int main() {
 
     int n = 16;
     int i = 0;
+    int no_of_itr = 0;
 
     while (i < n) {
+        no_of_itr++;
         size_t vl = __riscv_vsetvl_e32m1(n - i);
         vint32m1_t va = __riscv_vle32_v_i32m1(a + i, vl);
         vint32m1_t vb = __riscv_vle32_v_i32m1(b + i, vl);
@@ -19,6 +21,7 @@ int main() {
         i += vl;
     }
 
+    printf(" > No. of Iterations = %d\n", no_of_itr);
     printf("RVV vector add result:\n");
     for (int j = 0; j < n; j++) {
         printf("[ %s ] -> a[%2d] = %2d + 10 = c[%2d] = %2d\n",
